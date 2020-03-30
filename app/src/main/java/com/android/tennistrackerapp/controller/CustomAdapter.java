ce170cd20acde5553262f35bcb2c2588f1644e46
Ikka <daniel.ikka@edu.ece.fr>
2020-03-30 02:08:34 +0200

    First page designed
    - recycleView setup

---
+++ app/src/main/java/com/android/tennistrackerapp/controller/CustomAdapter.java
@@ -0,0 +1,66 @@
+package com.android.tennistrackerapp.controller;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.tennistrackerapp.R;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomViewHolder> {
+
+    private String[] mDataset;
+
+    // Provide a reference to the views for each data item (cell)
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    static class CustomViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView image;
+        private TextView title;
+
+        CustomViewHolder(View cell) {
+            super(cell);
+
+            //find cell's view
+            this.image = cell.findViewById(R.id.cell_ball_state);
+            this.title = cell.findViewById(R.id.cell_title);
+        }
+
+        void initWithData(String data) {
+            this.title.setText(data);
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public CustomAdapter(String[] dataSet) {
+        this.mDataset = dataSet;
+    }
+
+    @NonNull
+    @Override
+    public CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // create a new generic cell
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.cell_history, parent, false);
+
+        CustomViewHolder vh = new CustomViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CustomViewHolder holder, int position) {
+        String data = mDataset[position];
+        holder.initWithData(data);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+}
--- app/src/main/java/com/android/tennistrackerapp/controller/activities/HomeActivity.java
+++ app/src/main/java/com/android/tennistrackerapp/controller/activities/HomeActivity.java
@@ -1,17 +1,19 @@
 package com.android.tennistrackerapp.controller.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
 import android.os.Bundle;
 
 import com.android.tennistrackerapp.R;
+import com.android.tennistrackerapp.controller.CustomAdapter;
 import com.android.tennistrackerapp.controller.fragments.HeaderProfileFragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private Fragment mProfileFragment;
-
+    private HeaderProfileFragment mProfileFragment;
+    private RecyclerView mRv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +22,22 @@ public class HomeActivity extends AppCompatActivity {
 
         //FragmentManager
         initProfileFragment();
+
+        mRv = findViewById(R.id.recyclerView_main);
+        //RecycleView
+        //1- Layout Manager will manage displaying on screen, there it will be a vertical list
+        mRv.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
+        //2- The Adapter will manage content on each cell, it is a custom Class which extends of Adapter
+        mRv.setAdapter(new CustomAdapter(new String[]{
+                "Roger\nLieu - 07/07/2019",
+                "Nadal\nLieu - 07/07/2019",
+                "Roger\nLieu - 07/07/2019",
+                "Nadal\nLieu - 07/07/2019"}));
     }
 
     private void initProfileFragment() {
         // 1- Find fragment if already exist
-        this.mProfileFragment = getSupportFragmentManager().findFragmentById(R.id.frame_layout_header_profile);
+        this.mProfileFragment = (HeaderProfileFragment) getSupportFragmentManager().findFragmentById(R.id.frame_layout_header_profile);
 
         // 2- manage and do something if it's not the case
         if(this.mProfileFragment == null) {
@@ -35,7 +48,5 @@ public class HomeActivity extends AppCompatActivity {
                     .add(R.id.frame_layout_header_profile, this.mProfileFragment)
                     .commit();
         }
-
-
     }
 }
